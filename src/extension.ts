1939396c1c4aad89f64b5851a870c2baacc1640e
幸焕光 <xhg1945454120@gmail.com>
2020-10-11 19:51:25 +0800

    build(配置): 优化配置，增加eslint , prettier

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,39 @@
-import { window, ExtensionContext, ConfigurationChangeEvent, workspace, commands } from 'vscode';
-import { SwitchTheme } from './switchCore';
+import {
+  window,
+  ExtensionContext,
+  ConfigurationChangeEvent,
+  workspace,
+  commands,
+} from 'vscode'
+import { SwitchTheme } from './switchCore'
 
-const switchTheme = new SwitchTheme();
-
+const switchTheme = new SwitchTheme()
 
 function configChanged(event: ConfigurationChangeEvent) {
-  const sundialConfig = event.affectsConfiguration('switchTheme');
+  const sundialConfig = event.affectsConfiguration('switchTheme')
 
   if (sundialConfig) {
-    switchTheme.enableExtension();
+    switchTheme.enableExtension()
   }
 }
 
 export function activate(context: ExtensionContext) {
-  window.showInformationMessage('👏  自动切换Theme插件安装成功');
+  window.showInformationMessage('👏  自动切换Theme插件安装成功')
 
-  context.subscriptions.push(workspace.onDidChangeConfiguration(configChanged));
-  commands.registerCommand('switchTheme.closeSwitch', () => { 
-    window.showInformationMessage('🚫 关闭自动切换Theme插件');
+  context.subscriptions.push(
+    workspace.onDidChangeConfiguration(configChanged),
+  )
+  commands.registerCommand('switchTheme.closeSwitch', () => {
+    window.showInformationMessage('🚫 关闭自动切换Theme插件')
     switchTheme.disableExtension()
   })
-  commands.registerCommand('switchTheme.openSwitch', () => { 
-    window.showInformationMessage('✅ 开启切换Theme插件');
+  commands.registerCommand('switchTheme.openSwitch', () => {
+    window.showInformationMessage('✅ 开启切换Theme插件')
     switchTheme.enableExtension()
   })
-  switchTheme.enableExtension();
+  switchTheme.enableExtension()
 }
 
 export function deactivate() {
-	switchTheme.disableExtension();
+  switchTheme.disableExtension()
 }
-
--- src/switchCore.ts
+++ src/switchCore.ts
@@ -1,117 +1,123 @@
-import { WorkspaceConfiguration, ExtensionContext, window } from 'vscode';
-import dayjs from 'dayjs';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
-import * as editConfig from './configEdit';
+import {
+  WorkspaceConfiguration,
+  ExtensionContext,
+  window,
+} from 'vscode'
+import dayjs from 'dayjs'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+import * as editConfig from './configEdit'
 
 // 定义日出 | 日落接口
-export interface Sun { 
+export interface Sun {
   startTime: dayjs.Dayjs
   endTime: dayjs.Dayjs
 }
 
 // 定义分解后的切换配置接口
-interface splitSettingTime extends Sun{ 
+interface splitSettingTime extends Sun {
   useTheme: string
 }
 
 // 定义配置项
-export interface SwitchThemeConfig extends WorkspaceConfiguration { 
+export interface SwitchThemeConfig extends WorkspaceConfiguration {
   startTime: string
   switchThemeOptions: WorkspaceConfiguration
 }
 
 export class SwitchTheme {
-  
-  private enable = true;
-  private checkInterval!: NodeJS.Timer;
-  private isRunning = false;
+  private enable = true
+  private checkInterval!: NodeJS.Timer
+  private isRunning = false
   // 储存时间段
-  private timeList: Array<splitSettingTime> = [];
-  public static extensionContext: ExtensionContext;
+  private timeList: Array<splitSettingTime> = []
+  public static extensionContext: ExtensionContext
 
   // 扩展格式化时间
   constructor() {
-    dayjs.extend(customParseFormat);
+    dayjs.extend(customParseFormat)
   }
-  
+
   // 激活扩展
   public enableExtension() {
-    this.enable = true;
-    this.settingToggleTime();
-    this.getIntervalTime();
+    this.enable = true
+    this.settingToggleTime()
+    this.getIntervalTime()
   }
 
   // 关闭扩展
-  public disableExtension() { 
-    clearInterval(this.checkInterval);
-    this.enable = false;
+  public disableExtension() {
+    clearInterval(this.checkInterval)
+    this.enable = false
   }
 
   // 获取设置的间隔时间
   public getIntervalTime() {
-
-    const { editSwitchTheme } = editConfig.getConfig();
-    if (editSwitchTheme.interval === 0) { 
-      return;
+    const { editSwitchTheme } = editConfig.getConfig()
+    if (editSwitchTheme.interval === 0) {
+      return
     }
-    const interval = 1000 * editSwitchTheme.interval;
+    const interval = 1000 * editSwitchTheme.interval
     this.checkInterval = setInterval(() => {
-      this.checkOnTime();
-    }, interval);
+      this.checkOnTime()
+    }, interval)
   }
 
   // 获取当前时间
   public async getCurrentTime(): Promise<string> {
-    const { editSwitchTheme } = editConfig.getConfig();
+    const { editSwitchTheme } = editConfig.getConfig()
     // 获取当前时间
-    const now = dayjs();
-    let theTheme;
-    this.timeList.forEach(hitObj => {
-      let _start = now.isAfter(hitObj.startTime);
-      let _end = now.isBefore(hitObj.endTime);
-      if (_start && _end) { 
-        theTheme = hitObj.useTheme;
-        return;
+    const now = dayjs()
+    let theTheme
+    // eslint-disable-next-line prettier/prettier
+    this.timeList.forEach((hitObj) => {
+      const _start = now.isAfter(hitObj.startTime)
+      const _end = now.isBefore(hitObj.endTime)
+      if (_start && _end) {
+        theTheme = hitObj.useTheme
+        return
       }
-    });
-    return theTheme ? theTheme : editSwitchTheme.defaultTheme;
-
+    })
+    return theTheme ? theTheme : editSwitchTheme.defaultTheme
   }
 
   // 设置切换时间
   public settingToggleTime() {
-    const { editSwitchTheme} = editConfig.getConfig();
-    const { switchThemeOptions } = editSwitchTheme;
-    const startTime = dayjs(editSwitchTheme.startTime, 'HH:mm');
-    
-    if (this.timeList.length > 0) { 
-      this.timeList = [];
+    const { editSwitchTheme } = editConfig.getConfig()
+    const { switchThemeOptions } = editSwitchTheme
+    const startTime = dayjs(editSwitchTheme.startTime, 'HH:mm')
+
+    if (this.timeList.length > 0) {
+      this.timeList = []
     }
     switchThemeOptions.map((switchObj, index) => {
       // 处理fromTime转化为起始与结束区间
       if (switchObj.fromTime) {
-        switchObj.startTime = this.timeList.length > 0 ? this.timeList[index - 1].endTime : startTime;
-        switchObj.endTime = startTime.add(switchObj.fromTime, 'minute');
-        delete switchObj.fromTime;
-        this.timeList.push(switchObj);
+        switchObj.startTime =
+          this.timeList.length > 0
+            ? this.timeList[index - 1].endTime
+            : startTime
+        switchObj.endTime = startTime.add(
+          switchObj.fromTime,
+          'minute',
+        )
+        delete switchObj.fromTime
+        this.timeList.push(switchObj)
       }
-    });
+    })
   }
 
   // 检查是否需要切换Theme
-  public async checkOnTime() { 
+  public async checkOnTime() {
     if (!this.enable || this.isRunning) {
-      return;
+      return
     }
-    this.isRunning = true;
-    clearInterval(this.checkInterval);
-    
+    this.isRunning = true
+    clearInterval(this.checkInterval)
+
     // 获取到应该设置的主题
-    const currentTimeObj = await this.getCurrentTime();
-    editConfig.switchThemeHandle(currentTimeObj);
-    this.isRunning = false;
-    this.getIntervalTime();
-
+    const currentTimeObj = await this.getCurrentTime()
+    editConfig.switchThemeHandle(currentTimeObj)
+    this.isRunning = false
+    this.getIntervalTime()
   }
-
 }
